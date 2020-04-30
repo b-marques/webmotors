064327adf0b35ce913064174b8f5b9df56604ea2
Bruno Marques <brunomn95@gmail.com>
2020-04-30 12:19:25 -0300

    feat: change theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,8 @@
 export const theme = {
-  primary: '#f3123c',
-  secondary: '#808080',
-  secondary_variant_1: '#080808',
-  secondary_variant_2: '#9c9c9c',
-  secondary_variant_3: '#e8e8e8',
-  white: '#f8f8f8',
+  primary: '#ca242e',
+  secondary: '#2e2e37',
+  secondary_variant_1: '#4e4e4e',
+  secondary_variant_2: '#b6b6b6',
+  secondary_variant_3: '#dfdfdf',
+  white: '#fff',
 }
