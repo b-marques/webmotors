6f9017c7b39441cd7d84ef7ba6b0a302d620d4a0
Bruno Marques <brunomn95@gmail.com>
2020-04-29 22:25:57 -0300

    feat: add styled components initial theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  primary: '#f3123c',
+  secondary: '#808080',
+  secondary_variant_1: '#080808',
+  secondary_variant_2: '#9c9c9c',
+  secondary_variant_3: '#e8e8e8',
+  white: '#f8f8f8',
+}
