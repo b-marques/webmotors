bd05372ebed452d0c621c76dcec68461b98119f9
Bruno Marques <brunomn95@gmail.com>
2020-05-02 17:23:21 -0300

    refactor: adjust checkbox margins and padding

--- src/components/atoms/checkbox/Checkbox.style.ts
+++ src/components/atoms/checkbox/Checkbox.style.ts
@@ -14,18 +14,20 @@ export const Checkmark = styled.div`
   height: 1.3rem;
   border: 1px solid ${props => props.theme.secondaryVariant2};
   border-radius: 2px;
-  margin: 0 0.5rem;
+  margin: 0 0.8rem 0 0;
 `
 
 export const CheckIcon = styled.div`
   display: flex;
   color: ${props => props.theme.primary};
-  width: 60%;
-  height: 60%;
+  width: 73%;
+  height: 73%;
 
   & > svg {
     height: 100%;
   }
 `
 
-export const Label = styled.label``
+export const Label = styled.label`
+  padding: 0 3.5rem 0 0;
+`
