ca381e9b7fd8994d0a88f02f28eefd27d28b9121
Bruno Marques <brunomn95@gmail.com>
2020-05-03 00:24:07 -0300

    feat(component): add tab component

---
+++ src/components/atoms/tab/Tab.style.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const Container = styled.div<{ isActive: boolean }>`
+  max-width: 176.5px;
+  height: 73px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: start;
+  padding: 5px 20px 18px;
+  border-bottom: ${props =>
+    props.isActive
+      ? `3px solid${props.theme.primary}`
+      : `1px solid${props.theme.secondaryVariant2}`};
+  color: ${props => (props.isActive ? props.theme.primary : props.theme.secondaryVariant2)};
+`
+
+export const Icon = styled.div`
+  display: flex;
+  width: 100%;
+  height: 20px;
+  & > svg {
+    width: 25px;
+    height: 100%;
+  }
+`
+
+export const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const ActionText = styled.small`
+  vertical-align: bottom;
+  text-transform: uppercase;
+  font-size: 0.917rem;
+  color: ${props => props.theme.secondaryVariant2};
+  padding: 5px 0;
+`
+
+export const SubjectText = styled.div`
+  text-transform: uppercase;
+  font-size: 1.833rem;
+`
