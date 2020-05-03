1c599ecbdcb631f2b405b62745576dd0be92faa8
Bruno Marques <brunomn95@gmail.com>
2020-05-03 15:22:00 -0300

    refactor: increase checkbox label color

--- src/components/atoms/checkbox/Checkbox.style.ts
+++ src/components/atoms/checkbox/Checkbox.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  color: ${props => props.theme.secondaryVariant2};
+  color: ${props => props.theme.secondaryVariant1};
   cursor: pointer;
 `
 
