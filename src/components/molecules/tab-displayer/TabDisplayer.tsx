1dde04addb67d1c30c7f78a869d8bb51a91979a5
Bruno Marques <brunomn95@gmail.com>
2020-05-03 01:59:40 -0300

    feat(component): add tab displayer component

---
+++ src/components/molecules/tab-displayer/TabDisplayer.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from 'react'
+
+import { Container, Tabs, Content } from './TabDisplayer.style'
+import { Tab } from 'src/components/atoms/tab'
+
+type TabDisplayerProps = {
+  tabs: Array<React.ComponentProps<typeof Tab>>
+  content: Array<ReactNode>
+  activeTab: number
+  setActiveTab: (index: number) => void
+}
+
+export const TabDisplayer = (props: TabDisplayerProps) => {
+  const { tabs, content, activeTab, setActiveTab } = props
+  return (
+    <Container>
+      <Tabs>
+        {tabs.map((tab, index) => (
+          <Tab
+            key={`tab-${index}`}
+            icon={tab.icon}
+            actionText={tab.actionText}
+            subjectText={tab.subjectText}
+            isActive={index === activeTab}
+            onClick={() => setActiveTab(index)}
+          />
+        ))}
+      </Tabs>
+      <Content>{content[activeTab]}</Content>
+    </Container>
+  )
+}
