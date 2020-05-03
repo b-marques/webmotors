15d0f007329e1fceb275161571de3bfcac14fd90
Bruno Marques <brunomn95@gmail.com>
2020-05-03 01:58:37 -0300

    refactor: change dropdown/input reponsive sizes

--- src/components/molecules/input-concat-dropdown/InputConcatDropdown.style.ts
+++ src/components/molecules/input-concat-dropdown/InputConcatDropdown.style.ts
@@ -2,22 +2,20 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: 60% 40%;
+  grid-template-columns: 65% 35%;
 
   & > div:nth-child(1) {
-    flex-grow: 3;
     border-radius: 3px 0 0 3px;
   }
 
   & > div:nth-child(2) {
-    flex-grow: 1;
   }
 
   & > div:nth-child(2) > button {
     border-radius: 0 3px 3px 0;
   }
 
-  @media (min-width: 768px) {
-    grid-template-columns: 75% 25%;
+  @media (min-width: 992px) {
+    grid-template-columns: 73% 27%;
   }
 `
