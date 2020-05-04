9d60d4b628e5d83024809cf0f24ac0067e4047cf
Bruno Marques <brunomn95@gmail.com>
2020-05-04 17:09:36 -0300

    refactor: ensure tab displayer max height is 312px

--- src/components/molecules/tab-displayer/TabDisplayer.style.ts
+++ src/components/molecules/tab-displayer/TabDisplayer.style.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  @media (min-width: 768px) {
+    max-height: 312px;
+  }
+`
 
 export const Tabs = styled.div`
   display: flex;
