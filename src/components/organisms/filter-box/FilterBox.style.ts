66d6fe1239b6ef905cd2e32b51bd4289a7d3f84e
Bruno Marques <brunomn95@gmail.com>
2020-05-04 00:02:03 -0300

    refactor: add responsive style

--- src/components/organisms/filter-box/FilterBox.style.ts
+++ src/components/organisms/filter-box/FilterBox.style.ts
@@ -5,11 +5,16 @@ import { Card } from 'src/components/atoms/card/Card.style'
 export const Container = styled(Card)`
   display: grid;
   max-width: 933px;
-  max-height: 312px;
+  max-height: none;
   gap: 14px;
   grid-template-rows: repeat(3, auto) 1fr;
   grid-template-columns: repeat(8, 1fr);
-  padding: 27px 53px;
+  padding: 27px 27px;
+
+  @media (min-width: 768px) {
+    max-height: 239px;
+    padding: 27px 53px;
+  }
 `
 export const Checkboxes = styled.div`
   grid-column: 1 / span 8;
@@ -18,64 +23,107 @@ export const Checkboxes = styled.div`
 `
 
 export const Location = styled.div`
-  grid-column: 1 / span 4;
+  grid-column: 1 / span 8;
   grid-row: 2;
+
+  @media (min-width: 768px) {
+    grid-column: 1 / span 4;
+  }
 `
 
 export const Make = styled.div`
-  grid-column: 5 / span 2;
-  grid-row: 2;
+  grid-column: 1 / span 4;
+  grid-row: 4;
+
+  @media (min-width: 768px) {
+    grid-column: 5 / span 2;
+    grid-row: 2;
+  }
 `
 
 export const Model = styled.div`
-  grid-column: 7 / span 2;
-  grid-row: 2;
+  grid-column: 5 / span 4;
+  grid-row: 4;
+
+  @media (min-width: 768px) {
+    grid-column: 7 / span 2;
+    grid-row: 2;
+  }
 `
 
 export const Year = styled.div`
-  grid-column: 1 / span 2;
+  grid-column: 1 / span 4;
   grid-row: 3;
+
+  @media (min-width: 768px) {
+    grid-column: 1 / span 2;
+  }
 `
 
 export const Price = styled.div`
-  grid-column: 3 / span 2;
+  grid-column: 5 / span 4;
   grid-row: 3;
+
+  @media (min-width: 768px) {
+    grid-column: 3 / span 2;
+  }
 `
 
 export const Version = styled.div`
-  grid-column: 5 / span 4;
-  grid-row: 3;
+  grid-column: 1 / span 8;
+  grid-row: 5;
+
+  @media (min-width: 768px) {
+    grid-column: 5 / span 4;
+    grid-row: 3;
+  }
 `
 
 export const AdvancedSearch = styled.div`
-  grid-column: 1 / span 2;
-  grid-row: 4;
+  grid-column: 1 / span 4;
+  grid-row: 6;
   align-self: center;
   justify-self: left;
   text-justify: left;
-  margin-top: 7px;
 
   & > button {
     padding: 1rem 0;
+  }
+
+  @media (min-width: 768px) {
+    grid-column: 1 / span 2;
+    grid-row: 4;
+    margin-top: 7px;
   }
 `
 
 export const ClearFilter = styled.div`
-  grid-column: 5 / span 1;
-  grid-row: 4;
+  grid-column: 6 / span 3;
+  grid-row: 6;
   align-self: center;
-  justify-self: left;
-  text-justify: left;
-  margin-top: 7px;
+  justify-self: right;
 
   & > button {
     padding: 1rem 0;
   }
+
+  @media (min-width: 768px) {
+    grid-column: 5 / span 1;
+    grid-row: 4;
+    margin-top: 7px;
+    align-self: center;
+    justify-self: left;
+  }
 `
 
 export const MainButton = styled.div`
-  grid-column: 6 / span 3;
-  grid-row: 4;
+  grid-column: 1 / span 8;
+  grid-row: 7;
   align-self: center;
-  margin-top: 7px;
+
+  @media (min-width: 768px) {
+    grid-column: 6 / span 3;
+    grid-row: 4;
+    margin-top: 7px;
+  }
 `
