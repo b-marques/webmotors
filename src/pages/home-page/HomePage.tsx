aa06ec8ee28f06633f2b638811d59439d2162a5e
Bruno Marques <brunomn95@gmail.com>
2020-05-04 00:21:45 -0300

    refactor: move sample items to separeted file

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -9,36 +9,11 @@ import { HomeTemplate } from 'src/components/templates/home-template'
 import { Navbar } from 'src/components/atoms/navbar'
 import { Card } from 'src/components/atoms/card'
 import { Example } from 'src/utils/samples/exampleTabContent'
+import { priceItems, radiusItems, yearItems } from 'src/utils/samples/items'
 
 const API_URL = process.env.REACT_APP_API_URL || ''
 
 export const HomePage = () => {
-  const radiusItems = [
-    { id: 1, name: '50km' },
-    { id: 2, name: '100km' },
-    { id: 3, name: '150km' },
-    { id: 4, name: '200km' },
-    { id: 5, name: '400km' },
-    { id: 6, name: '1000km' },
-  ]
-
-  const yearItems = [
-    { id: 1, name: '1995' },
-    { id: 2, name: '2000' },
-    { id: 3, name: '2005' },
-    { id: 4, name: '2012' },
-    { id: 5, name: '2018' },
-    { id: 6, name: '2020' },
-  ]
-  const priceItems = [
-    { id: 1, name: '0,00 ~ 5.000,00' },
-    { id: 2, name: '5.001,00 ~ 10.000,00' },
-    { id: 3, name: '10.001,00 ~ 20.000,00' },
-    { id: 4, name: '20.001,00 ~ 40.000,00' },
-    { id: 5, name: '40.001,00 ~ 80.000,00' },
-    { id: 6, name: 'acima de 80.000,00' },
-  ]
-
   const [activeTab, setActiveTab] = useState(0)
   const [isNew, setIsNew] = useState(false)
   const [isUsed, setIsUsed] = useState(false)
---
+++ src/utils/samples/items.ts
@@ -0,0 +1,27 @@
+export const radiusItems = [
+  { id: 1, name: '50km' },
+  { id: 2, name: '100km' },
+  { id: 3, name: '150km' },
+  { id: 4, name: '200km' },
+  { id: 5, name: '400km' },
+  { id: 6, name: '1000km' },
+]
+
+export const yearItems = [
+  { id: 1, name: '1995' },
+  { id: 1, name: '1996' },
+  { id: 2, name: '2000' },
+  { id: 3, name: '2005' },
+  { id: 4, name: '2012' },
+  { id: 5, name: '2018' },
+  { id: 6, name: '2020' },
+]
+
+export const priceItems = [
+  { id: 1, name: '0,00 ~ 5.000,00' },
+  { id: 2, name: '5.001,00 ~ 10.000,00' },
+  { id: 3, name: '10.001,00 ~ 20.000,00' },
+  { id: 4, name: '20.001,00 ~ 40.000,00' },
+  { id: 5, name: '40.001,00 ~ 80.000,00' },
+  { id: 6, name: 'acima de 80.000,00' },
+]
