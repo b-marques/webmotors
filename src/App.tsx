32692d3e9524644c5f815391e2f6b8169fd5c6f0
Bruno Marques <brunomn95@gmail.com>
2020-05-04 00:07:27 -0300

    refator: change root App component
    - Wrap project on theme provider
    
    - Add global style
    
    - Add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
-import logo from './logo.svg'
-import './App.css'
+import { GlobalStyle } from 'src/styles/globalStyle'
+import { theme } from 'src/styles/theme'
+import { HomePage } from 'src/pages/home-page'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <HomePage />
+    </ThemeProvider>
   )
 }
 
