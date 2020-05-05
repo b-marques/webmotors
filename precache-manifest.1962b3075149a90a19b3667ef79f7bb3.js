e480a239d321d1c258865f56d60bd5c86fa41d6d
Bruno Marques <brunomn95@gmail.com>
2020-05-04 23:36:05 -0300

    Updates

---
+++ precache-manifest.1962b3075149a90a19b3667ef79f7bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c9c64ce73942eea01e0274d47ed596",
+    "url": "/webmotors/index.html"
+  },
+  {
+    "revision": "36909dc72123c8f3b448",
+    "url": "/webmotors/static/js/2.0e2af60c.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/webmotors/static/js/2.0e2af60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc53c98c75f1859956d8",
+    "url": "/webmotors/static/js/main.5076d2e8.chunk.js"
+  },
+  {
+    "revision": "e377d0e048e8b7b4409a",
+    "url": "/webmotors/static/js/runtime-main.0b2e2132.js"
+  },
+  {
+    "revision": "ef770bc7dcbd611d1eaf7be5bcdb7863",
+    "url": "/webmotors/static/media/logo.ef770bc7.svg"
+  }
+]);
